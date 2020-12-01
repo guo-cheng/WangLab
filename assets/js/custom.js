03d933cee9947aec6aef76fe3e167d3cdf850df2
guo-cheng <guochengli666@gmail.com>
2020-12-01 20:21:08 +0800

    Initial commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,5 +7,3 @@
 
   gtag('config', 'G-3HDBGZ7V15');
 </script>
-
-<script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=5fkfz7vhfr6&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;f=arial&amp;l=33" async="async"></script>
